7a8eaebd63a5dcb9deaf96f11d1b23957892d8e3
Maricruz Alvarado <maricruzalvaradoc@outlook.com>
2025-02-17 13:20:11 -0600

    agregar option null 13

--- src/hooks/useGetCatologoPaseNoJwt.ts
+++ src/hooks/useGetCatologoPaseNoJwt.ts
@@ -2,8 +2,55 @@
 import { getCatalogosPaseNoJwt } from "@/lib/get-catalogos_pase_no_jwt";
 import { useQuery } from "@tanstack/react-query";
 
+
+export interface Root {
+  response: Response
+  success: boolean
+  log: string
+}
+
+export interface Response {
+  data: Data
+}
+
+export interface Data {
+  pass_selected: PassSelected
+  cat_estados: string[]
+  cat_vehiculos: string[]
+}
+
+export interface PassSelected {
+  folio: string
+  estatus: string
+  grupo_equipos: any[]
+  grupo_vehiculos: any[]
+  fecha_de_caducidad: string
+  fecha_de_expedicion: string
+  ubicacion: string
+  nombre: string
+  visita_a: VisitaA[]
+  _id: string
+  telefono: string
+  email: string
+  qr_pase: QrPase[]
+}
+
+export interface VisitaA {
+  puesto: string
+  nombre: string
+  user_id: number
+  email: string
+  departamento: string
+}
+
+export interface QrPase {
+  file_name: string
+  file_url: string
+  file: string
+}
+
 export const useGetCatalogoPaseNoJwt = (account_id:number, qr_code:string ) => {
-  const { data: data, isLoading, error, isFetching, refetch } = useQuery<any>({
+  const { data: data, isLoading, error, isFetching, refetch } = useQuery<Response>({
     queryKey: ["useGetCatalogoPaseNoJwt"], 
     queryFn: async () => {
         const data = await getCatalogosPaseNoJwt(account_id, qr_code); 
